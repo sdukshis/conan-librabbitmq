d5bd6d8a971190e41dd59cfe67dd46f105d3897c
Pavel Filonov <pavel.filonov@kaspersky.com>
2016-08-10 16:24:39 +0300

    mingw_configurations

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="filonovpv", archs=["x86_64"])
+    mingw_configurations = [("4.9", "x86_64", "seh", "posix"),
+                            ("4.9", "x86_64", "sjlj", "posix"),
+                            ("4.9", "x86", "sjlj", "posix"),
+                            ("4.9", "x86", "dwarf2", "posix")]
+    builder = ConanMultiPackager(mingw_configurations=mingw_configurations,
+                                 archs=["x86_64"])
     builder.add_common_builds(pure_c=True)
     builder.run()
