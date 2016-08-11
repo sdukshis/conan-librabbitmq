4276fd5a086b7fd38efecd76defdd92ac5d15a7b
Pavel Filonov <pavel.filonov@kaspersky.com>
2016-08-11 09:25:28 +0300

    disable mingw build configurations

--- build.py
+++ build.py
@@ -1,11 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    mingw_configurations = [("4.9", "x86_64", "seh", "posix"),
-                            ("4.9", "x86_64", "sjlj", "posix"),
-                            ("4.9", "x86", "sjlj", "posix"),
-                            ("4.9", "x86", "dwarf2", "posix")]
-    builder = ConanMultiPackager(mingw_configurations=mingw_configurations,
-                                 archs=["x86_64"])
+    builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds(pure_c=True)
     builder.run()
