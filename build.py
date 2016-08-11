77eba23a278a1bf71e39f2b87effd251749779a5
Pavel Filonov <pavel.filonov@kaspersky.com>
2016-08-11 09:30:51 +0300

    amend mingw builds

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"])
+    builder = ConanMultiPackager(mingw_configurations=[],
+                                 archs=["x86_64"])
     builder.add_common_builds(pure_c=True)
     builder.run()
